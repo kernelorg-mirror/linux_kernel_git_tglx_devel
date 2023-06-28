a70b261cf476374f6883aa21e03ed89694b69e34
Thomas Gleixner <tglx@linutronix.de>
2023-06-28 15:21:20 +0200

    x86/cpu: Move cpu_die_id into topology info
    Move the next member.
    
    No functional change.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -82,6 +82,7 @@ struct cpuinfo_topology {
 	u16			initial_apicid;
 
 	u16			pkg_id;
+	u16			die_id;
 };
 
 struct cpuinfo_x86 {
@@ -137,7 +138,6 @@ struct cpuinfo_x86 {
 	u16			logical_proc_id;
 	/* Core id: */
 	u16			cpu_core_id;
-	u16			cpu_die_id;
 	u16			logical_die_id;
 	/* Index into per_cpu list: */
 	u16			cpu_index;
