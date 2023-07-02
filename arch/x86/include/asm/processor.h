604e7ab0813f8111062a4f57d38412953ffa6dfd
Thomas Gleixner <tglx@linutronix.de>
2023-07-02 11:50:26 +0200

    x86/cpu: Move cpu_l[l2]c_id into topology info
    The topology IDs which identify the LLC and L2 domains clearly belong to
    the per CPU topology information.
    
    Move them into cpuinfo_x86::cpuinfo_topo and get rid of the extra per CPU
    data and the related exports.
    
    This also paves the way to do proper topology evaluation during early boot
    because it removes the only per CPU dependency for that.
    
    No functional change.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -96,6 +96,10 @@ struct cpuinfo_topology {
 	// Logical ID mappings
 	u16			logical_pkg_id;
 	u16			logical_die_id;
+
+	// Cache level topology IDs
+	u16			llc_id;
+	u16			l2c_id;
 };
 
 struct cpuinfo_x86 {
@@ -685,7 +689,15 @@ extern int set_tsc_mode(unsigned int val);
 
 DECLARE_PER_CPU(u64, msr_misc_features_shadow);
 
-extern u16 get_llc_id(unsigned int cpu);
+static inline u16 per_cpu_llc_id(unsigned int cpu)
+{
+	return per_cpu(cpu_info.topo.llc_id, cpu);
+}
+
+static inline u16 per_cpu_l2c_id(unsigned int cpu)
+{
+	return per_cpu(cpu_info.topo.l2c_id, cpu);
+}
 
 #ifdef CONFIG_CPU_SUP_AMD
 extern u32 amd_get_nodes_per_socket(void);
