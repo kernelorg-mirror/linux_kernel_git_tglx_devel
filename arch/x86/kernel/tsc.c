49600d43307b405ae0fa89b4a1a67e17055500fa
Thomas Gleixner <tglx@kernel.org>
2026-01-22 22:40:30 +0100

    x86/tsc: Handle CLOCK_SOURCE_VALID_FOR_HRES correctly
    Unconditionally setting the CLOCK_SOURCE_VALID_FOR_HRES for the real TSC
    clocksource is wrong as there is no guarantee that the early TSC was
    validated for high resolution mode.
    
    Set the flag only when the early TSC was validated as otherwise the
    clocksource selection might enable high resolution mode with a TSC of
    unknown quality and possibly no way to back out once it is discovered to be
    unsuitable.
    
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Cc: x86@kernel.org

--- arch/x86/kernel/tsc.c
+++ arch/x86/kernel/tsc.c
@@ -1193,7 +1193,6 @@ static struct clocksource clocksource_tsc = {
 	.read			= read_tsc,
 	.mask			= CLOCKSOURCE_MASK(64),
 	.flags			= CLOCK_SOURCE_IS_CONTINUOUS |
-				  CLOCK_SOURCE_VALID_FOR_HRES |
 				  CLOCK_SOURCE_MUST_VERIFY |
 				  CLOCK_SOURCE_VERIFY_PERCPU,
 	.id			= CSID_X86_TSC,
@@ -1403,6 +1402,15 @@ out:
 		have_art = true;
 		clocksource_tsc.base = &art_base_clk;
 	}
+
+	/*
+	 * Transfer the valid for high resolution flag if it was set on the
+	 * early TSC already. That guarantees that there is no intermediate
+	 * clocksource selected once the early TSC is unregistered.
+	 */
+	if (clocksource_tsc_early.flags & CLOCK_SOURCE_VALID_FOR_HRES)
+		clocksource_tsc.flags |= CLOCK_SOURCE_VALID_FOR_HRES;
+
 	clocksource_register_khz(&clocksource_tsc, tsc_khz);
 unreg:
 	clocksource_unregister(&clocksource_tsc_early);
