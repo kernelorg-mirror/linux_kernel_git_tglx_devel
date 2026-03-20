3390f40fd6036b41fe455b908495029014ce7c99
Thomas Gleixner <tglx@kernel.org>
2026-03-20 14:22:35 +0100

    x86/irq: Hook up architecture specific stats
    Enable the binary statistics interface for architecture specific
    interrupts.
    
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Link: https://patch.msgid.link/20260320132102.977658755@kernel.org

--- arch/x86/include/asm/hardirq.h
+++ arch/x86/include/asm/hardirq.h
@@ -65,7 +65,14 @@ DECLARE_PER_CPU_SHARED_ALIGNED(irq_cpustat_t, irq_stat);
 #ifdef CONFIG_X86_POSTED_MSI
 DECLARE_PER_CPU_ALIGNED(struct pi_desc, posted_msi_pi_desc);
 #endif
+
 #define __ARCH_IRQ_STAT
+#define ARCH_IRQ_STATS_NUM_IRQS		IRQ_COUNT_MAX
+
+static inline unsigned int arch_get_irq_stat(unsigned int cpu, unsigned int idx)
+{
+	return data_race(per_cpu_ptr(&irq_stat, cpu)->counts[idx]);
+}
 
 #define inc_irq_stat(index)	this_cpu_inc(irq_stat.counts[IRQ_COUNT_##index])
 
