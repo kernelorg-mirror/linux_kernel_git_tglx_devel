75c525fc64d61c91f571d5f9b582f12218d024ea
Thomas Gleixner <tglx@kernel.org>
2026-03-26 22:56:48 +0100

    x86/irq: Make irqstats array based
    Having the x86 specific interrupt statistics as a data structure with
    individual members instead of an array is just stupid as it requires
    endless copy and paste in arch_show_interrupts() and arch_irq_stat_cpu(),
    where the latter does not even take the latest interrupt additions into
    account. The resulting #ifdef orgy is just disgusting.
    
    Convert it to an array of counters, which does not make a difference in the
    actual interrupt hotpath increment as the array index is constant and
    therefore not any different than the member based access.
    
    But in arch_show_interrupts() and arch_irq_stat_cpu() this just turns into
    a loop, which reduces the text size by ~2k (~12%):
    
       text	   data	    bss	    dec	    hex	filename
      19643	  15250	    904	  35797	   8bd5	../build/arch/x86/kernel/irq.o
      17355	  15250	    904	  33509	   82e5	../build/arch/x86/kernel/irq.o
    
    Adding a new vector or software counter only requires to update the table
    and everything just works. Using the core provided emit function which
    speeds up 0 outputs makes it significantly faster.
    
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Reviewed-by: Radu Rendec <radu@rendec.net>

--- arch/x86/kernel/apic/apic.c
+++ arch/x86/kernel/apic/apic.c
@@ -1040,7 +1040,7 @@ static void local_apic_timer_interrupt(void)
 	/*
 	 * the NMI deadlock-detector uses this.
 	 */
-	inc_irq_stat(apic_timer_irqs);
+	inc_irq_stat(APIC_TIMER);
 
 	evt->event_handler(evt);
 }
@@ -2108,7 +2108,7 @@ static noinline void handle_spurious_interrupt(u8 vector)
 
 	trace_spurious_apic_entry(vector);
 
-	inc_irq_stat(irq_spurious_count);
+	inc_irq_stat(SPURIOUS);
 
 	/*
 	 * If this is a spurious interrupt then do not acknowledge
