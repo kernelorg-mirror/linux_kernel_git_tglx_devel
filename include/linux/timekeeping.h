2d5ec49fcb4a1f89cc028671bd8fb802abbee1c3
Thomas Gleixner <tglx@kernel.org>
2026-05-22 10:03:42 +0200

    timekeeping: Provide ktime_get_snapshot_id()
    ktime_get_snapshot() provides a snapshot of the underlying clocksource
    counter value and the corresponding CLOCK_MONOTONIC_RAW, CLOCK_REALTIME and
    CLOCK_BOOTTIME timestamps.
    
    There is no usage of CLOCK_REALTIME and CLOCK_BOOTTIME at the same time and
    CLOCK_BOOTTIME support was just added for the ARM64 KVM tracing mechanism,
    which needs CLOCK_BOOTTIME and the underlying clocksource counter value.
    
    ktime_get_snapshot() is also not suitable for usage with CLOCK_AUX, but
    that's a prerequisite to support PTP hardware timestamping for CLOCK_AUX
    steering.
    
    As a first step, rename ktime_get_snapshot() to ktime_get_snapshot_id(),
    which now takes a clockid argument to select the clock which needs to be
    captured. The result is stored in system_time_snapshot::sys, which will
    replace the system_time_snapshot::real/boot members once all usage sites
    have been converted.
    
    ktime_get_snapshot() is a simple wrapper which hands in CLOCK_REALTIME as
    clockid argument for the conversion period. That means CLOCK_REALTIME is
    now captured twice, but that redunancy is only temporary.
    
    No functional change vs. current users of ktime_get_snapshot()
    
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>

--- include/linux/timekeeping.h
+++ include/linux/timekeeping.h
@@ -276,24 +276,28 @@ static inline bool ktime_get_aux_ts64(clockid_t id, struct timespec64 *kt) { ret
 #endif
 
 /**
- * struct system_time_snapshot - simultaneous raw/real time capture with
- *				 counter value
- * @cycles:	Clocksource counter value to produce the system times
- * @real:	Realtime system time
- * @boot:	Boot time
- * @raw:	Monotonic raw system time
- * @cs_id:	Clocksource ID
+ * struct system_time_snapshot - Simultaneous time capture of CLOCK_MONOTONIC_RAW,
+ *				 a selected CLOCK_* and the clocksource counter value
+ * @cycles:		Clocksource counter value to produce the system times
+ * @sys:		The system time of the selected CLOCK ID
+ * @real:		Realtime system time
+ * @boot:		Boot time
+ * @raw:		Monotonic raw system time
+ * @cs_id:		Clocksource ID
  * @clock_was_set_seq:	The sequence number of clock-was-set events
  * @cs_was_changed_seq:	The sequence number of clocksource change events
+ * @valid:		True if the snapshot is valid
  */
 struct system_time_snapshot {
 	u64			cycles;
+	ktime_t			sys;
 	ktime_t			real;
 	ktime_t			boot;
 	ktime_t			raw;
 	enum clocksource_ids	cs_id;
 	unsigned int		clock_was_set_seq;
 	u8			cs_was_changed_seq;
+	u8			valid;
 };
 
 /**
@@ -341,9 +345,16 @@ extern int get_device_system_crosststamp(
 			struct system_device_crosststamp *xtstamp);
 
 /*
- * Simultaneously snapshot realtime and monotonic raw clocks
+ * Simultaneously snapshot a given clock with MONOTONIC_RAW and the underlying
+ * clocksource counter value.
  */
-extern void ktime_get_snapshot(struct system_time_snapshot *systime_snapshot);
+extern bool ktime_get_snapshot_id(struct system_time_snapshot *systime_snapshot,
+				  clockid_t clock_id);
+
+static inline void ktime_get_snapshot(struct system_time_snapshot *systime_snapshot)
+{
+	WARN_ON_ONCE(!ktime_get_snapshot_id(systime_snapshot, CLOCK_REALTIME));
+}
 
 /*
  * Persistent clock related interfaces
