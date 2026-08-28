4bfaaed17c82c7f2190b8297fb2d1ae28d6ece42
Thomas Gleixner <tglx@kernel.org>
2026-08-28 15:47:29 +0200

    x86: Define __ARCH_IRQ_EXIT_IRQS_DISABLED
    Signed-off-by: Thomas Gleixner <tglx@kernel.org

--- arch/x86/include/asm/hardirq.h
+++ arch/x86/include/asm/hardirq.h
@@ -113,4 +113,6 @@ static __always_inline bool kvm_get_cpu_l1tf_flush_l1d(void)
 static __always_inline void kvm_set_cpu_l1tf_flush_l1d(void) { }
 #endif /* IS_ENABLED(CONFIG_KVM_INTEL) */
 
+#define __ARCH_IRQ_EXIT_IRQS_DISABLED 1
+
 #endif /* _ASM_X86_HARDIRQ_H */
